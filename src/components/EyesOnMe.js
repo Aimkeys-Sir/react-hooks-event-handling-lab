d439ac3607648ac798c1cbd315dd9d2d6831416a
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-07-13 21:21:45 +0300

    commit tree 399ff8b96042ef705153cf6204d98ed9e2b97a47

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+export default function EyesOnMe(){
+    function isEyesOnMe(){
+        console.log("Good!")
+    }
+    function isEyesOffMe(){
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <button onBlur={isEyesOffMe} onFocus={isEyesOnMe}>Eyes on me</button>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+export default function Keypad(){
+    function printItOut(event){
+        console.log(`Entering password...`)
+    }
+return (
+    <form>
+        <label htmlFor="inputP">Enter Password</label>
+        <input id="inputP" onChange={printItOut} type={"password"}/>
+    </form>
+)
+}
